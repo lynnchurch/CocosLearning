c5edc45ac07f7b115fdd3f26fda936698c9d3445
lynnchurch <lynnchurch@163.com>
2016-04-04 01:11:28 +0800

    commit files

---
+++ HelloCC/Classes/HelloWorldScene.h
@@ -0,0 +1,24 @@
+#ifndef __HELLOWORLD_SCENE_H__
+#define __HELLOWORLD_SCENE_H__
+
+#include "cocos2d.h"
+
+class HelloWorld : public cocos2d::Layer
+{
+protected:
+	float _angle;
+	cocos2d::Vec2 _direction;
+
+public:
+	static cocos2d::Scene* createScene();
+
+	virtual bool init();
+
+	// a selector callback
+	void menuCloseCallback(cocos2d::Ref* pSender);
+
+	// implement the "static create()" method manually
+	CREATE_FUNC(HelloWorld);
+};
+
+#endif // __HELLOWORLD_SCENE_H__
