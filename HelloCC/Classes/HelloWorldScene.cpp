4e1eb1e11546ac6ec42c1644f4d1f7e0b6e98621
lynnchurch <lynnchurch@163.com>
2016-04-06 23:37:58 +0800

    finished

--- HelloCC/Classes/HelloWorldScene.cpp
+++ HelloCC/Classes/HelloWorldScene.cpp
@@ -30,63 +30,21 @@ bool HelloWorld::init()
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-	//   // 正方形
-	//auto rect = DrawNode::create();
-	//rect->drawRect(Vec2(0, 0), Vec2(150, 150), Color4F(1, 0, 0, 1));
-	//rect->setPosition(visibleSize / 2);
-	//rect->setContentSize(Size(150, 150));
-	//rect->setAnchorPoint(Vec2(0.5, 0.5));
-
-	//// 点
-	//auto dot = DrawNode::create();
-	//dot->drawDot(Vec2(0, 0),10,Color4F(1,1,1,1));
-
-	//schedule([rect, dot](float f){
-	//	rect->setRotation(rect->getRotation() + 1);
-	//	auto p = dot->convertToWorldSpace(Vec2(0, 0));
-	//	CCLOG("%f,%f", p.x, p.y);
-	//}, "Test");
-
-	//rect->addChild(dot);
-	//addChild(rect);
-
-
-	//auto dot = DrawNode::create();
-	//dot->drawDot(Vec2(0, 0), 10, Color4F(1, 1, 1, 1));
-	//dot->setPosition(visibleSize / 2);
-	//addChild(dot);
-
-	//_angle = 0;
-	//schedule([dot,visibleSize,this](float f) {
-	//	dot->setPositionY(visibleSize.height/2+cos(_angle) * 100);
-	//	dot->setPositionX(visibleSize.width / 2 + sin(_angle) * 60);
-	//	_angle+=0.1;
-	//}, "Test");
-
-	_direction.set(random(-1, 1), random(-1, 1));
-	_direction.normalize();
-
-	auto dot = DrawNode::create();
-	dot->drawDot(Vec2(10, 10), 10, Color4F(1, 1, 1, 1));
-	dot->setContentSize(Size(20, 20));
-	dot->setAnchorPoint(Vec2(0.5, 0.5));
-	dot->setPosition(visibleSize / 2);
-	addChild(dot);
-
-	schedule([dot, visibleSize, this](float f) {
-		auto p = dot->getPosition();
-		if (p.x<10 || p.x>visibleSize.width - 10)
-		{
-			_direction.x *= -1;
-		}
-		else if (p.y<10 || p.y>visibleSize.height - 10)
-		{
-			_direction.y *= -1;
-		}
-		dot->setPosition(p + _direction);
-
-	}, "Test");
-
+	auto zombie = Sprite::create("zombie.png");
+	zombie->setPosition(Vec2(visibleSize.width/6,visibleSize.height/2));
+	zombie->setScale(1.1);
+	addChild(zombie);
+	//zombie->runAction(MoveTo::create(5,Vec2(visibleSize.width-50,visibleSize.height/2)));
+	//auto action1 = MoveTo::create(5, Vec2(visibleSize.width - 50, visibleSize.height / 2));
+	//auto action2 = MoveTo::create(3, Vec2(100, visibleSize.height / 2));
+	auto move1 = MoveBy::create(5, Vec2(300, 0));
+	//auto move2 = move1->reverse();
+	auto scale = ScaleTo::create(2, 1.3);
+	//zombie->runAction(Spawn::create(move1, scale, NULL));
+	auto spawn = Spawn::create(move1, scale, NULL);
+	zombie->runAction(Sequence::create(spawn, CallFunc::create([](){
+		CCLOG("Animation completed.");
+	}), NULL));
 	return true;
 }
 
