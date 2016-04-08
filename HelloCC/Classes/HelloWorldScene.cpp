ad19a811e3d0e96c2c5beb63fb58c3121357b7fc
lynnchurch <lynnchurch@163.com>
2016-04-08 23:28:53 +0800

    finished

--- HelloCC/Classes/HelloWorldScene.cpp
+++ HelloCC/Classes/HelloWorldScene.cpp
@@ -4,17 +4,17 @@ USING_NS_CC;
 
 Scene* HelloWorld::createScene()
 {
-    // 'scene' is an autorelease object
-    auto scene = Scene::create();
-    
-    // 'layer' is an autorelease object
-    auto layer = HelloWorld::create();
+	// 'scene' is an autorelease object
+	auto scene = Scene::create();
 
-    // add layer as a child to scene
-    scene->addChild(layer);
+	// 'layer' is an autorelease object
+	auto layer = HelloWorld::create();
 
-    // return the scene
-    return scene;
+	// add layer as a child to scene
+	scene->addChild(layer);
+
+	// return the scene
+	return scene;
 }
 
 // on "init" you need to initialize your instance
@@ -30,72 +30,92 @@ bool HelloWorld::init()
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-	//   // 正方形
-	//auto rect = DrawNode::create();
-	//rect->drawRect(Vec2(0, 0), Vec2(150, 150), Color4F(1, 0, 0, 1));
-	//rect->setPosition(visibleSize / 2);
-	//rect->setContentSize(Size(150, 150));
-	//rect->setAnchorPoint(Vec2(0.5, 0.5));
-
-	//// 点
-	//auto dot = DrawNode::create();
-	//dot->drawDot(Vec2(0, 0),10,Color4F(1,1,1,1));
-
-	//schedule([rect, dot](float f){
-	//	rect->setRotation(rect->getRotation() + 1);
-	//	auto p = dot->convertToWorldSpace(Vec2(0, 0));
-	//	CCLOG("%f,%f", p.x, p.y);
-	//}, "Test");
-
-	//rect->addChild(dot);
-	//addChild(rect);
-
-
-	//auto dot = DrawNode::create();
-	//dot->drawDot(Vec2(0, 0), 10, Color4F(1, 1, 1, 1));
-	//dot->setPosition(visibleSize / 2);
-	//addChild(dot);
-
-	//_angle = 0;
-	//schedule([dot,visibleSize,this](float f) {
-	//	dot->setPositionY(visibleSize.height/2+cos(_angle) * 100);
-	//	dot->setPositionX(visibleSize.width / 2 + sin(_angle) * 60);
-	//	_angle+=0.1;
-	//}, "Test");
-
-	_direction.set(random(-1, 1), random(-1, 1));
-	_direction.normalize();
-
-	auto dot = DrawNode::create();
-	dot->drawDot(Vec2(10, 10), 10, Color4F(1, 1, 1, 1));
-	dot->setContentSize(Size(20, 20));
-	dot->setAnchorPoint(Vec2(0.5, 0.5));
-	dot->setPosition(visibleSize / 2);
-	addChild(dot);
-
-	schedule([dot, visibleSize, this](float f) {
-		auto p = dot->getPosition();
-		if (p.x<10 || p.x>visibleSize.width - 10)
-		{
-			_direction.x *= -1;
-		}
-		else if (p.y<10 || p.y>visibleSize.height - 10)
-		{
-			_direction.y *= -1;
-		}
-		dot->setPosition(p + _direction);
+	/*auto sprite = Sprite::create("HelloWorld.png");
+	sprite->setPosition(visibleSize / 2);
+	addChild(sprite);
+	CCLOG("sprite size(1):%f %f", sprite->getContentSize().width, sprite->getContentSize().height);
+	schedule([sprite](float f){
+		CCLOG("sprite size:%f %f", sprite->getContentSize().width, sprite->getContentSize().height);
 
 	}, "Test");
+	//sprite->retain();*/
 
+	/*Vector <Label *> vec;
+	auto label1 = Label::create();
+	label1->setString("Label 1");
+	label1->setSystemFontSize(30);
+	auto label2 = Label::create();
+	label2->setString("Label 2");
+	label2->setSystemFontSize(30);
+	vec.pushBack(label1);
+	vec.pushBack(label2);
+	int i = 0;
+	for (auto label : vec)
+	{
+		i++;
+		if (1 == i)
+		{
+			label->setPosition(Vec2(visibleSize.width / 5, visibleSize.height / 2));
+		}
+		else if (2 == i)
+		{
+			label->setPosition(Vec2(visibleSize.width * 4 / 5, visibleSize.height / 2));
+		}
+		addChild(label);
+	}*/
+
+	/*Map<std::string, Label *> map;
+	auto label1 = Label::create();
+	label1->setString("Label 1");
+	label1->setSystemFontSize(30);
+	label1->setPosition(Vec2(visibleSize.width / 5, visibleSize.height / 2));
+	auto label2 = Label::create();
+	label2->setString("Label 2");
+	label2->setSystemFontSize(30);
+	label2->setPosition(Vec2(visibleSize.width * 4 / 5, visibleSize.height / 2));
+	map.insert("label1", label1);
+	map.insert("label2", label2);
+	addChild(map.at("label1"));
+	addChild(map.at("label2"));*/
+
+	Value v1 = Value(123);
+	Value v2 = Value("abc");
+	Value v3 = Value(true);
+	CCLOG("%s %s %s", v1.asString().c_str(), v2.asString().c_str(), v3.asString().c_str());
+
+	Value v4 = Value::Null;
+	v4 = v1;
+	v4 = v2;
+	v4 = v3;
+	CCLOG("%s %s", v4.getType() == Value::Type::BOOLEAN ? "True" : "False", v4.asString().c_str());
+
+	ValueVector vec;
+	vec.push_back(Value("Hello"));
+	vec.push_back(v1);
+	vec.push_back(v2);
+	vec.push_back(v3);
+	for (auto value : vec)
+	{
+		CCLOG("ValueVector %s", value.asString().c_str());
+	}
+
+	ValueMap map;
+	map["a"] = v1;
+	map["b"] = v2;
+	map["c"] = v3;
+	for (auto m : map)
+	{
+		CCLOG("%s:%s", m.first.c_str(), m.second.asString().c_str());
+	}
 	return true;
 }
 
 
 void HelloWorld::menuCloseCallback(Ref* pSender)
 {
-    Director::getInstance()->end();
+	Director::getInstance()->end();
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
+	exit(0);
 #endif
 }
