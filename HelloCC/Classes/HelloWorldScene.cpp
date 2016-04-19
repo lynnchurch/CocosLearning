f8b60f1bafbe528df6dcbfb663826857f42ae39e
lynnchurch <lynnchurch@163.com>
2016-04-20 01:36:30 +0800

    finished

--- HelloCC/Classes/HelloWorldScene.cpp
+++ HelloCC/Classes/HelloWorldScene.cpp
@@ -1,20 +1,21 @@
 #include "HelloWorldScene.h"
-
+#include "tinyxml2/tinyxml2.h"
+#include "json/document.h"
 USING_NS_CC;
 
 Scene* HelloWorld::createScene()
 {
-    // 'scene' is an autorelease object
-    auto scene = Scene::create();
-    
-    // 'layer' is an autorelease object
-    auto layer = HelloWorld::create();
+	// 'scene' is an autorelease object
+	auto scene = Scene::create();
 
-    // add layer as a child to scene
-    scene->addChild(layer);
+	// 'layer' is an autorelease object
+	auto layer = HelloWorld::create();
 
-    // return the scene
-    return scene;
+	// add layer as a child to scene
+	scene->addChild(layer);
+
+	// return the scene
+	return scene;
 }
 
 // on "init" you need to initialize your instance
@@ -30,72 +31,64 @@ bool HelloWorld::init()
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-	//   // 正方形
-	//auto rect = DrawNode::create();
-	//rect->drawRect(Vec2(0, 0), Vec2(150, 150), Color4F(1, 0, 0, 1));
-	//rect->setPosition(visibleSize / 2);
-	//rect->setContentSize(Size(150, 150));
-	//rect->setAnchorPoint(Vec2(0.5, 0.5));
+	// 首选项的读写
+	/*UserDefault::getInstance()->setStringForKey("data", "Hello Lynn!");
+	auto data = UserDefault::getInstance()->getStringForKey("data", "Hello World!");
+	MessageBox(data.c_str(), "Message");*/
 
-	//// 点
-	//auto dot = DrawNode::create();
-	//dot->drawDot(Vec2(0, 0),10,Color4F(1,1,1,1));
+	// 文件的读写
+	//auto fu = FileUtils::getInstance();
+	//CCLOG("file path:%s", fu->getWritablePath().c_str());
+	//auto filePath = fu->fullPathFromRelativeFile("data.txt", fu->getWritablePath());
+	///*auto file = fopen(filePath.c_str(), "w");
+	//fprintf(file, "Hello Lynn.\n");
+	//fclose(file);*/
+	//auto data=fu->getDataFromFile(filePath);
+	//CCLOG("file content:%s", data.getBytes());
 
-	//schedule([rect, dot](float f){
-	//	rect->setRotation(rect->getRotation() + 1);
-	//	auto p = dot->convertToWorldSpace(Vec2(0, 0));
-	//	CCLOG("%f,%f", p.x, p.y);
-	//}, "Test");
+	// plist文件的读取
+	/*auto fu = FileUtils::getInstance();
+	auto vm = fu->getValueMapFromFile("user_info.plist");
+	auto jack = vm["jack"].asValueMap();
+	CCLOG("jack's phone_num:%s",jack["phone_num"].asString().c_str());*/
 
-	//rect->addChild(dot);
-	//addChild(rect);
+	// xml文件的读取
+	/*auto doc = new tinyxml2::XMLDocument();
+	doc->Parse(FileUtils::getInstance()->getStringFromFile("user_info.xml").c_str());
+	auto root = doc->RootElement();
+	for (auto e = root->FirstChildElement(); e; e = e->NextSiblingElement())
+	{
+	std::string s;
+	for (auto attr = e->FirstAttribute(); attr; attr = attr->Next())
+	{
+	s+=attr->Name();
+	s += ":";
+	s += attr->Value();
+	s += " ";
+	}
+	CCLOG("people:%s\n", s.c_str());
+	}*/
 
-
-	//auto dot = DrawNode::create();
-	//dot->drawDot(Vec2(0, 0), 10, Color4F(1, 1, 1, 1));
-	//dot->setPosition(visibleSize / 2);
-	//addChild(dot);
-
-	//_angle = 0;
-	//schedule([dot,visibleSize,this](float f) {
-	//	dot->setPositionY(visibleSize.height/2+cos(_angle) * 100);
-	//	dot->setPositionX(visibleSize.width / 2 + sin(_angle) * 60);
-	//	_angle+=0.1;
-	//}, "Test");
-
-	_direction.set(random(-1, 1), random(-1, 1));
-	_direction.normalize();
-
-	auto dot = DrawNode::create();
-	dot->drawDot(Vec2(10, 10), 10, Color4F(1, 1, 1, 1));
-	dot->setContentSize(Size(20, 20));
-	dot->setAnchorPoint(Vec2(0.5, 0.5));
-	dot->setPosition(visibleSize / 2);
-	addChild(dot);
-
-	schedule([dot, visibleSize, this](float f) {
-		auto p = dot->getPosition();
-		if (p.x<10 || p.x>visibleSize.width - 10)
-		{
-			_direction.x *= -1;
-		}
-		else if (p.y<10 || p.y>visibleSize.height - 10)
-		{
-			_direction.y *= -1;
-		}
-		dot->setPosition(p + _direction);
-
-	}, "Test");
-
+	// json文件的读取
+	rapidjson::Document doc;
+	doc.Parse<0>(FileUtils::getInstance()->getStringFromFile("user_info.json").c_str());
+	std::string str = doc[(int)0]["name"].GetString();
+	str += ":";
+	str += doc[(int)0]["age"].GetString();
+	str += "\n";
+	str += doc[1]["name"].GetString();
+	str += ":";
+	str += doc[1]["age"].GetString();
+	CCLOG(str.c_str());
 	return true;
 }
 
 
 void HelloWorld::menuCloseCallback(Ref* pSender)
 {
-    Director::getInstance()->end();
+	Director::getInstance()->end();
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
+	exit(0);
 #endif
 }
