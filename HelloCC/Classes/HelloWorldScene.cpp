4b37bd4baad229e304b6e9b9994c651c02e5413b
lynnchurch <lynnchurch@163.com>
2016-04-23 16:10:11 +0800

    finished

--- HelloCC/Classes/HelloWorldScene.cpp
+++ HelloCC/Classes/HelloWorldScene.cpp
@@ -4,17 +4,17 @@ USING_NS_CC;
 
 Scene* HelloWorld::createScene()
 {
-    // 'scene' is an autorelease object
-    auto scene = Scene::create();
-    
-    // 'layer' is an autorelease object
-    auto layer = HelloWorld::create();
+	// 'scene' is an autorelease object
+	auto scene = Scene::create();
 
-    // add layer as a child to scene
-    scene->addChild(layer);
+	// 'layer' is an autorelease object
+	auto layer = HelloWorld::create();
 
-    // return the scene
-    return scene;
+	// add layer as a child to scene
+	scene->addChild(layer);
+
+	// return the scene
+	return scene;
 }
 
 // on "init" you need to initialize your instance
@@ -30,62 +30,63 @@ bool HelloWorld::init()
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-	//   // 正方形
-	//auto rect = DrawNode::create();
-	//rect->drawRect(Vec2(0, 0), Vec2(150, 150), Color4F(1, 0, 0, 1));
-	//rect->setPosition(visibleSize / 2);
-	//rect->setContentSize(Size(150, 150));
-	//rect->setAnchorPoint(Vec2(0.5, 0.5));
+	auto map = TMXTiledMap::create("map.tmx");
+	addChild(map);
 
-	//// 点
-	//auto dot = DrawNode::create();
-	//dot->drawDot(Vec2(0, 0),10,Color4F(1,1,1,1));
+	// 获取地图属性
+	auto mapPro = map->getProperties();
+	CCLOG("map pro:%s", mapPro["type"].asString().c_str());
 
-	//schedule([rect, dot](float f){
-	//	rect->setRotation(rect->getRotation() + 1);
-	//	auto p = dot->convertToWorldSpace(Vec2(0, 0));
-	//	CCLOG("%f,%f", p.x, p.y);
-	//}, "Test");
+	// 获取图层属性
+	auto layerPro = map->getLayer("scene")->getProperties();
+	CCLOG("layer pro:%s", layerPro["type"].asString().c_str());
 
-	//rect->addChild(dot);
-	//addChild(rect);
+	// 获取图块属性
+	auto graphPro = map->getPropertiesForGID(2).asValueMap();
+	CCLOG("graph pro:%s", graphPro["type"].asString().c_str());
 
+	// 获取对象属性
+	auto objPro = map->getObjectGroup("object")->getProperties();
+	CCLOG("obj pro:%s", objPro["type"].asString().c_str());
 
-	//auto dot = DrawNode::create();
-	//dot->drawDot(Vec2(0, 0), 10, Color4F(1, 1, 1, 1));
-	//dot->setPosition(visibleSize / 2);
-	//addChild(dot);
+	// 修改指定区域的颜色
+	auto colorObj = map->getObjectGroup("object")->getObject("color");
+	auto colorObjX = colorObj["x"].asInt();
+	auto colorObjY = colorObj["y"].asInt();
+	auto colorObjW = colorObj["width"].asInt();
+	auto colorObjH = colorObj["height"].asInt();
+	auto rect = DrawNode::create();
+	rect->drawSolidRect(Vec2(colorObjX, colorObjY), Vec2(colorObjX + colorObjW, colorObjY + colorObjH), Color4F(Color3B(255, 0, 0)));
+	addChild(rect);
 
-	//_angle = 0;
-	//schedule([dot,visibleSize,this](float f) {
-	//	dot->setPositionY(visibleSize.height/2+cos(_angle) * 100);
-	//	dot->setPositionX(visibleSize.width / 2 + sin(_angle) * 60);
-	//	_angle+=0.1;
-	//}, "Test");
+	// 加载小球
+	auto player = map->getObjectGroup("object")->getObject("player");
+	auto boll = Sprite::create("c.png");
+	boll->setPosition(Vec2(player["x"].asFloat(), player["y"].asFloat()));
+	addChild(boll);
 
-	_direction.set(random(-1, 1), random(-1, 1));
-	_direction.normalize();
-
-	auto dot = DrawNode::create();
-	dot->drawDot(Vec2(10, 10), 10, Color4F(1, 1, 1, 1));
-	dot->setContentSize(Size(20, 20));
-	dot->setAnchorPoint(Vec2(0.5, 0.5));
-	dot->setPosition(visibleSize / 2);
-	addChild(dot);
-
-	schedule([dot, visibleSize, this](float f) {
-		auto p = dot->getPosition();
-		if (p.x<10 || p.x>visibleSize.width - 10)
+	// 瓦片的碰撞检测
+	auto listener = EventListenerTouchOneByOne::create();
+	listener->onTouchBegan = [boll, map](Touch *t, Event *e)
+	{
+		auto point = map->convertToNodeSpace(t->getLocation());
+		auto tileX = static_cast<int>(point.x / 32);
+		auto tileY = map->getMapSize().height - static_cast<int>(point.y / 32) - 1;
+		auto gid = map->getLayer("scene")->getTileGIDAt(Vec2(tileX, tileY));
+		auto gidPro = map->getPropertiesForGID(gid);
+		CCLOG("gid:%d", gid);
+		if (!gidPro.isNull())
 		{
-			_direction.x *= -1;
+			auto pro = map->getPropertiesForGID(gid).asValueMap();
+			if (!pro["movable"].isNull() && pro["movable"].asBool())
+			{
+				boll->setPosition(point);
+			}
 		}
-		else if (p.y<10 || p.y>visibleSize.height - 10)
-		{
-			_direction.y *= -1;
-		}
-		dot->setPosition(p + _direction);
-
-	}, "Test");
+		
+		return true;
+	};
+	Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, this);
 
 	return true;
 }
@@ -93,9 +94,9 @@ bool HelloWorld::init()
 
 void HelloWorld::menuCloseCallback(Ref* pSender)
 {
-    Director::getInstance()->end();
+	Director::getInstance()->end();
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
+	exit(0);
 #endif
 }
